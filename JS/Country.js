acc1fe753c679dfdd8fbc5e9ed37c711d076acbb
Tucker Triggs <tucker.triggs@yahoo.com>
2017-10-28 17:12:55 +0200

    cleaned up countries object

--- JS/Country.js
+++ JS/Country.js
@@ -1,18 +1,3 @@
-/**
- * Created by Sallar Kaboli <sallar.kaboli@gmail.com>
- * License: WTFL <http://www.wtfpl.net>
- * ---
- * http://sallar.me
- */
-/**--A Pen created at CodePen.io. You can find this one at http://codepen.io/tuckpuck/pen/gMpMBR. Forked from [Nicholas Petersen](http://codepen.io/nicholaspetersen/)'s Pen [NPejZZ](http://codepen.io/nicholaspetersen/pen/NPejZZ/).-->
-
-          <!--
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--->*/
 //part 1 - https://s3-us-west-2.amazonaws.com/s.cdpn.io/35376/d3.v3.min.js//
 d3 = function () {
     function n(n) {
@@ -4375,7 +4360,7 @@ d3 = function () {
         return "#" + ct(this.r) + ct(this.g) + ct(this.b)
     };
     var so = ya.map({
-        
+
     });
     so.forEach(function (n, t) {
         so.set(n, it(t))
